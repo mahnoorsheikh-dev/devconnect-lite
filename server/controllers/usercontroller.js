9ad1538e60c99fe045b6c9d80b4e08ac244162c3
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-04-02 12:12:38 -0700

    Save data in MongoDB and make password hashed

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 exports.registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -8,7 +11,15 @@ exports.registerUser = async (req, res) => {
       return res.status(400).json({ message: 'Please provide all fields' });
     }
 
-    res.status(201).json({ message: 'User registered successfully' });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create ({
+       name,
+       email,
+       password: hashedPassword
+       });
+
+    res.status(201).json(user);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Server error" });
