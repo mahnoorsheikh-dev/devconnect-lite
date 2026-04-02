a11fc27d0d44e885a6d628e55d32b0b671ce1c81
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-04-02 06:42:27 -0700

    user controller add try catch method to extend it later

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,14 +1,16 @@
 exports.registerUser = async (req, res) => {
-
+  try {
     const { name, email, password } = req.body;
-
     console.log(name, email, password);
 
     // Basic validation
     if (!name || !email || !password) {
       return res.status(400).json({ message: 'Please provide all fields' });
-    } else {
-      return res.status(200).json({ message: 'User registered successfully' }); 
     }
-}
 
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
