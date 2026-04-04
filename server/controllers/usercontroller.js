c16e877184d832bb1385d33ca658cca03096a2a0
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-04-04 14:03:41 -0700

    User Login logic implement successfully

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -25,3 +25,26 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please provide email and password' });
+    }     
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser} = require("../controllers/usercontroller");
+const {registerUser, loginUser} = require("../controllers/usercontroller");
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
