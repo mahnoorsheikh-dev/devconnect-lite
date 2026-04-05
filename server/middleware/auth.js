b79ceef34a732c32e2a25823fe3bdc00f4e78662
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-04-05 12:20:21 -0700

    Token auth middleware check

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+exports.authentication = (req, res, next) => {
+   const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  } 
+
+  req.token = token;
+  
+  next();
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {registerUser, loginUser} = require("../controllers/usercontroller");
+const {authentication} = require("../middleware/auth");
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.get('/profile', authentication, (req, res) => {
+  res.status(200).json({ message: 'Protected route accessed', user: req.token });
+});
 
 module.exports = router;
